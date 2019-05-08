a2d9f03509ac875de9031290b7722805a2dcf16b
Rufino Rojas <lucras015@hotmail.com>
2019-05-08 18:30:37 -0500

    se agrego likes

--- src/app/post/post.page.ts
+++ src/app/post/post.page.ts
@@ -1,6 +1,11 @@
-import { AngularFirestore } from "@angular/fire/firestore";
+import {
+  AngularFirestore,
+  AngularFirestoreDocument
+} from "@angular/fire/firestore";
 import { Component, OnInit } from "@angular/core";
 import { ActivatedRoute } from "@angular/router";
+import { UserService } from "../user.service";
+import { firestore } from "firebase/app";
 
 @Component({
   selector: "app-post",
@@ -10,19 +15,49 @@ import { ActivatedRoute } from "@angular/router";
 export class PostPage implements OnInit {
   postID: string;
   post;
+  postReference: AngularFirestoreDocument;
+  sub;
+
   heartType: string = "heart-empty";
 
-  constructor(private route: ActivatedRoute, private afs: AngularFirestore) {}
+  constructor(
+    private route: ActivatedRoute,
+    private afs: AngularFirestore,
+    private user: UserService
+  ) {}
 
   ngOnInit() {
-    this.postID = this.route.snapshot.paramMap.get('id');
-    this.post = this.afs.doc(`posts/${this.postID}`).valueChanges();
+    this.postID = this.route.snapshot.paramMap.get("id");
+    this.postReference = this.afs.doc(`posts/${this.postID}`);
+    this.sub = this.postReference.valueChanges().subscribe(val => {
+      this.post = val;
+      this.heartType = val.likes.includes(this.user.getUID())
+        ? "heart"
+        : "heartEmpty";
+    });
 
     console.log(this.post);
     console.log(this.postID);
   }
 
+  ngOnDestroy() {
+    //Called once, before the instance is destroyed.
+    //Add 'implements OnDestroy' to the class.
+
+    this.sub.unsubscribe();
+  }
+
   like() {
-    this.heartType = this.heartType == "heart" ? "heart-empty" : "heart";
+    //this.heartType = this.heartType == "heart" ? "heart-empty" : "heart";
+
+    if (this.heartType == "heart-empty") {
+      this.postReference.update({
+        likes: firestore.FieldValue.arrayUnion(this.user.getUID())
+      });
+    } else {
+      this.postReference.update({
+        likes: firestore.FieldValue.arrayRemove(this.user.getUID())
+      });
+    }
   }
 }
--- src/app/uploader/uploader.page.ts
+++ src/app/uploader/uploader.page.ts
@@ -1,8 +1,10 @@
-import { AngularFirestore } from '@angular/fire/firestore';
+import { Router } from '@angular/router';
+import { AlertController } from '@ionic/angular';
+import { AngularFirestore } from "@angular/fire/firestore";
 import { Component, OnInit, ViewChild } from "@angular/core";
-import { Http} from '@angular/http';
-import { UserService } from '../user.service';
-import { firestore } from 'firebase/app';
+import { Http } from "@angular/http";
+import { UserService } from "../user.service";
+import { firestore } from "firebase/app";
 
 @Component({
   selector: "app-uploader",
@@ -12,18 +14,22 @@ import { firestore } from 'firebase/app';
 export class UploaderPage implements OnInit {
   imageURL: string;
   descripcion: string = "";
+  busy: boolean = false;
 
+  @ViewChild("filebtn") filebtn;
 
-
-  @ViewChild('filebtn') filebtn;
-
-  constructor(private http: Http,
+  constructor(
+    private http: Http,
     private afstore: AngularFirestore,
-    private user:UserService) {}
+    private user: UserService,
+    private alertController: AlertController,
+    private router: Router
+  ) {}
 
   ngOnInit() {}
 
   fileChange(event) {
+    this.busy = true;
     const files = event.target.files;
 
     const data = new FormData();
@@ -36,25 +42,44 @@ export class UploaderPage implements OnInit {
       .subscribe(event => {
         console.log(event);
         this.imageURL = event.json().file;
+        console.log('Esta es la url ' + this.imageURL);
       });
+
+    this.busy = false;
   }
-  createPost() {
+  async createPost() {
+    this.busy = true;
     const image = this.imageURL;
     const desc = this.descripcion;
 
     this.afstore.doc(`users/${this.user.getUID()}`).update({
-      post: firestore.FieldValue.arrayUnion(image),
+      post: firestore.FieldValue.arrayUnion(image)
     });
 
     this.afstore.doc(`posts/${image}`).set({
       desc,
       autor: this.user.getName(),
-      likes:[]
+      likes: []
     });
+    this.busy = false;
+    this.imageURL = "";
+    this.descripcion = "";
+
+    const alert = await this.alertController.create({
+      header: "success",
+      message: "Post creado!",
+      buttons: ["Ok"]
+    });
+    await alert.present();
+    this.router.navigate(["/tabs/feed"]);
   }
 
-  uploadFile(){
+  uploadFile() {
     this.filebtn.nativeElement.click();
   }
-  
+
+  cancelar(){
+     this.imageURL = "";
+     this.descripcion = "";
+  }
 }
